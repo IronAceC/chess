d81c16668e1348093f1194b0679ff1e0133abac3
IronAceC <105246900+IronAceC@users.noreply.github.com>
2023-10-25 10:06:58 -0600

    Add files via upload

---
+++ Chess2/Pieces/pawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess2.Pieces
+{
+    class Pawn : Fatherpiece
+    {
+        static void Raycast(char[,] board, string move)
+        {
+            Console.WriteLine("how");
+        }
+    }
+}
