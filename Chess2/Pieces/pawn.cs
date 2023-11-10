f6733e2d8bdace706529753d8d2c712e69aebe20
IronAceC <105246900+IronAceC@users.noreply.github.com>
2023-11-10 09:02:29 -0600

    Add files via upload

--- Chess2/Pieces/pawn.cs
+++ Chess2/Pieces/pawn.cs
@@ -10,8 +10,27 @@ namespace Chess2.Pieces
     {
         public static bool Raycast(char[,] board, string move)
         {
-            Console.WriteLine("how");
-            return true;
+            int startX = move[0] - 'a';
+            int startY = '8' - move[1];
+            int endX = move[3] - 'a';
+            int endY = '8' - move[4];
+            Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+            if (startY == 6 && (endY == 4 || endY == 5))
+            {
+                return true;
+            }
+            else if (startY != 6 && Char.IsWhiteSpace(board[startY - 1, startX]) && endY == (startY - 1) && endX == startX)
+            {
+                return true;
+            }
+            else if ((Char.IsLetter(board[startY - 1, startX - 1]) || Char.IsLetter(board[startY -1, startX + 1])) && (endY == startY -1 && (endX == startX + 1 || endX == startX -1)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
