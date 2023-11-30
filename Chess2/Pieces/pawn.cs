e004f88d464b65a3e0512e3babc20ef75afc541f
IronAceC <105246900+IronAceC@users.noreply.github.com>
2023-11-30 10:18:09 -0600

    Add files via upload

--- Chess2/Pieces/pawn.cs
+++ Chess2/Pieces/pawn.cs
@@ -15,19 +15,19 @@ namespace Chess2.Pieces
             int endX = move[3] - 'a';
             int endY = '8' - move[4];
             Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
-            if (startY == 6 && (endY == 4 || endY == 5))
+            if (startY == 6 && (endY == 4 || endY == 5))//allows piece to jump two on first turn
             {
                 return true;
             }
-            else if (startY != 6 && Char.IsWhiteSpace(board[startY - 1, startX]) && endY == (startY - 1) && endX == startX)
+            else if (startY != 6 && Char.IsWhiteSpace(board[startY - 1, startX]) && endY == (startY - 1) && endX == startX)//every other turn, one space forward
             {
                 return true;
             }
             else if ((Char.IsLetter(board[startY - 1, startX - 1]) || Char.IsLetter(board[startY -1, startX + 1])) && (endY == startY -1 && (endX == startX + 1 || endX == startX -1)))
-            {
+            {//allows attack on diagonal
                 return true;
             }
-            else
+            else//i decided to not add en passant, i would need to learn how to add classes :P
             {
                 return false;
             }
--- Chess2/Pieces/rook.cs
+++ Chess2/Pieces/rook.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace Chess2.Pieces
 {
-    internal class rook
+    public class Rook : Fatherpiece
     {
+        public static bool Raycast(char[,] board, string move)
+        {
+            int startX = move[0] - 'a';
+            int startY = '8' - move[1];
+            int endX = move[3] - 'a';
+            int endY = '8' - move[4];
+
+            if (startX != endX || startY == endY)//moving horizontally
+            {
+                if(startX > endX)//moving left
+                {
+                    for (int i = startX - 1; i >= endX; i = i - 1)
+                    {
+                        Console.WriteLine(i);
+                        Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+                        if (Char.IsLetter(board[startY, i]) == true)
+                        {
+                            if (endX < i)
+                            {
+                                Console.WriteLine("1");
+                                return false;
+                            }
+                            else if (endX == i)
+                            {
+                                Console.WriteLine("2");
+                                return true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("3");
+                                return false;
+                            }
+                        }
+                        else if (Char.IsWhiteSpace(board[startY, i]))
+                        {
+                            Console.WriteLine("4");
+                            return true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("5");
+                            return false;
+                        }
+                    }
+                    return true;
+
+                }
+                else if (startX < endX)//moving right
+                {
+                    return true;
+                }
+                else { return false; }
+
+            }
+            else if (startY != endY || startX == endX)//moving vertically
+            {
+                if (startY > endY)//moving down
+                {
+                    return true;
+                }
+
+                else if (startY < endY)//moving up
+                {
+                    return true;
+                }
+                else {return false; }
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
