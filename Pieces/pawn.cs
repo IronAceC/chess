bd40aad71e1ea02c835db70f403a05cb1e648675
IronAceC <105246900+IronAceC@users.noreply.github.com>
2024-07-03 18:52:22 -0600

    Add files via upload

---
+++ Pieces/pawn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess2.Pieces
+{
+    public class Pawn : Fatherpiece
+    {
+        public static bool Raycast(char[,] board, string move)
+        {
+            int startX = move[0] - 'a';
+            int startY = '8' - move[1];
+            int endX = move[3] - 'a';
+            int endY = '8' - move[4];
+            //Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+            if (startY == 6 && (endY == 4 || endY == 5))//allows piece to jump two on first turn
+            {
+                return true;
+            }
+            else if (startY != 6 && Char.IsWhiteSpace(board[startY - 1, startX]) && endY == (startY - 1) && endX == startX)//every other turn, one space forward
+            {
+                return true;
+            }
+            else if (startX == 0)
+            {
+                if (Char.IsLetter(board[startY - 1, startX + 1]) && (endY == startY - 1 && (endX == startX + 1)))
+                {//allows attack on diagonal
+                    return true;
+                }
+                else { return false; }
+            }
+            else if (startX == 8)
+            {
+                if ((Char.IsLetter(board[Math.Abs(startY - 1), Math.Abs(startX - 1)]) && (endY == startY - 1 && endX == startX - 1)))
+                {//allows attack on diagonal
+                    return true;
+                }
+                else { return false; }
+            }
+            else if ((Char.IsLetter(board[Math.Abs(startY - 1), Math.Abs(startX - 1)]) || Char.IsLetter(board[startY -1, startX + 1])) && (endY == startY -1 && (endX == startX + 1 || endX == startX -1)))
+            {//allows attack on diagonal
+                return true;
+            }
+            else//
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Pieces/rook.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess2.Pieces
+{
+    public class Rook : Fatherpiece
+    {
+        public static bool Raycast(char[,] board, string move)
+        {
+            int startX = move[0] - 'a';
+            int startY = '8' - move[1];
+            int endX = move[3] - 'a';
+            int endY = '8' - move[4];
+
+            if (startX != endX && startY == endY)//moving horizontally
+            {
+                if(startX > endX)//moving left
+                {
+                    bool mayI = false;
+                    for (int i = startX - 1; i >= endX; i--)//looking ahead until we reach desired x
+                    {
+                        //Console.WriteLine(i);
+                        //Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+                        if (Char.IsLetter(board[startY, i]))//if thing ahead is a letter...
+                        {
+                            if (endX == i)//it's okay to replace the piece...
+                            {
+                                mayI = true;
+                            }
+                            else if (endX < i)//but not to go beyond it
+                            {
+                                mayI = false;
+                            }
+                            break;
+                        }
+                        else if (Char.IsWhiteSpace(board[startY, i]))//and we can keep going until there is nothing
+                        {
+                            mayI = true;
+                        }
+                        
+                    }
+                    if (mayI == false)
+                    {
+                        return false;
+                    }
+                    else { return true; }
+                    
+
+                }
+                else if (startX < endX)//moving right
+                {
+                    bool mayI = false;
+                    for (int i = startX + 1; i <= endX; i++)//looking ahead until we reach desired x
+                    {
+                        //Console.WriteLine(i);
+                        //Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+                        if (Char.IsLetter(board[startY, i]))//if thing ahead is a letter...
+                        {
+                            if (endX == i)//it's okay to replace the piece...
+                            {
+                                mayI = true;
+                            }
+                            else if (endX > i)//but not to go beyond it
+                            {
+                                mayI = false;
+                            }
+                            break;
+                        }
+                        else if (Char.IsWhiteSpace(board[startY, i]))//and we can keep going until there is nothing
+                        {
+                            mayI = true;
+                        }
+
+                    }
+                    if (mayI == false)
+                    {
+                        return false;
+                    }
+                    else { return true; }
+                }
+                else { return false; }
+
+            }
+            else if (startY != endY && startX == endX)//moving vertically
+            {
+                if (startY > endY)//moving down
+                {
+                    bool mayI = false;
+                    for (int i = startY - 1; i >= endY; i--)//looking ahead until we reach desired y
+                    {
+                        //Console.WriteLine(i);
+                        //Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+                        if (Char.IsLetter(board[i, startX]))//if thing ahead is a letter...
+                        {
+                            if (endY == i)//it's okay to replace the piece...
+                            {
+                                mayI = true;
+                            }
+                            else if (endY < i)//but not to go beyond it
+                            {
+                                mayI = false;
+                            }
+                            break;
+                        }
+                        else if (Char.IsWhiteSpace(board[i, startX]))//and we can keep going until there is nothing
+                        {
+                            mayI = true;
+                        }
+
+                    }
+                    if (mayI == false)
+                    {
+                        return false;
+                    }
+                    else { return true; }
+                }
+
+                else if (startY < endY)//moving up
+                {
+                    bool mayI = false;
+                    for (int i = startY + 1; i >= endY; i++)//looking ahead until we reach desired y
+                    {
+                        //Console.WriteLine(i);
+                        //Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
+                        if (Char.IsLetter(board[i, startX]))//if thing ahead is a letter...
+                        {
+                            if (endY == i)//it's okay to replace the piece...
+                            {
+                                mayI = true;
+                            }
+                            else if (endY < i)//but not to go beyond it
+                            {
+                                mayI = false;
+                            }
+                            break;
+                        }
+                        else if (Char.IsWhiteSpace(board[i, startX]))//and we can keep going until there is nothing
+                        {
+                            mayI = true;
+                        }
+
+                    }
+                    if (mayI == false)
+                    {
+                        return false;
+                    }
+                    else { return true; }
+                }
+                else {return false; }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
