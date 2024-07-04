037ccfbabf00ec87c6f8996286a04f66ff9cf6b0
IronAceC <105246900+IronAceC@users.noreply.github.com>
2024-07-04 16:32:14 -0600

    Add files via upload

--- Pieces/pawn.cs
+++ Pieces/pawn.cs
@@ -8,12 +8,13 @@ namespace Chess2.Pieces
 {
     public class Pawn : Fatherpiece
     {
-        public static bool Raycast(char[,] board, string move)
+        public static bool Raycast(char[,] board, string move,ref (double,int) passantable)
         {
             int startX = move[0] - 'a';
             int startY = '8' - move[1];
             int endX = move[3] - 'a';
             int endY = '8' - move[4];
+             
             //Console.WriteLine(startX + " " + startY + " " + endX + " " + endY);
             if (startY == 6 && (endY == 4 || endY == 5))//allows piece to jump two on first turn
             {
@@ -29,21 +30,139 @@ namespace Chess2.Pieces
                 {//allows attack on diagonal
                     return true;
                 }
-                else { return false; }
+                else if (passantable != (9, 9))
+                {
+                    (double, int) pboard = (startY, startX + 1);
+                    if ((board[startY, (startX + 1)]) == 'p' && pboard == passantable)
+                    {
+                        
+                        if (pboard == passantable)
+                        {
+                            if (endX == startX - 1 && endY == startY + 1)
+                            {
+                                passantable = (11, startX + 1);
+                                 
+                                return true;
+                            }
+                            else return false;
+                        }
+                        else return false;
+                    }
+                    else { return false; }
+                }
+                else return false;
             }
-            else if (startX == 8)
+            else if (startX == 7)
             {
                 if ((Char.IsLetter(board[Math.Abs(startY - 1), Math.Abs(startX - 1)]) && (endY == startY - 1 && endX == startX - 1)))
                 {//allows attack on diagonal
                     return true;
                 }
-                else { return false; }
+                else if (passantable != (9, 9))
+                {
+                    (double, int) pboard = (startY, startX - 1);
+                    if ((board[startY, (startX - 1)]) == 'p' && pboard == passantable)
+                    {
+                        
+                        if (pboard == passantable)
+                        {
+                            if (endX == startX - 1 && endY == startY - 1)
+                            {
+                                passantable = (11, startX - 1);
+                                 
+                                return true;
+                            }
+                            else return false;
+                        }
+                        else { return false; }
+                    }
+                    else return false;
+                }
+                else return false;
             }
-            else if ((Char.IsLetter(board[Math.Abs(startY - 1), Math.Abs(startX - 1)]) || Char.IsLetter(board[startY -1, startX + 1])) && (endY == startY -1 && (endX == startX + 1 || endX == startX -1)))
+            else if ((Char.IsLetter(board[Math.Abs(startY - 1), Math.Abs(startX - 1)]) || Char.IsLetter(board[startY - 1, startX + 1])) && (endY == startY - 1 && (endX == startX + 1 || endX == startX - 1)))
             {//allows attack on diagonal
                 return true;
             }
-            else//
+            else if (passantable != (9,9))
+            {
+
+                if (startX == 0)
+                {
+                    if ((board[startY, (startX + 1)]) == 'p')
+                    {
+                        (double, int) pboard = (startY, startX + 1);
+                        if (pboard == passantable)
+                        {
+                            if (endX == startX - 1 && endY == startY + 1)
+                            {
+                                passantable = (11,startX + 1);
+                                 
+                                return true;
+                            }
+                            else return false;
+                        }
+                        else return false;
+                    }
+                    else return false;
+                }
+                else if (startX == 7)
+                {
+                    if ((board[startY, (startX - 1)]) == 'p')
+                    {
+                        (double, int) pboard = (startY, startX - 1);
+                        if (pboard == passantable)
+                        {
+                            if (endX == startX - 1 && endY == startY - 1)
+                            {
+                                passantable = (11, startX - 1);
+                                 
+                                return true;
+                            }
+                            else return false;
+                        }
+                        else { return false; }
+                    }
+                    else return false;
+                }
+                else
+                {
+                    (double, int) pboard = (startY, startX - 1);
+                    if ((board[startY, (startX - 1)]) == 'p' && pboard == passantable)
+                    {
+                        if (pboard == passantable)
+                        {
+                            if (endX == startX - 1 && endY == startY - 1)
+                            {
+                                passantable = (11, startX - 1);
+                                 
+                                return true;
+                            }
+                            else return false;
+                        }
+                        else return false;
+                    }
+                    else if ((board[startY, (startX + 1)]) == 'p' && (startY , startX + 1) == passantable)
+                    {
+                        if ((startY , startX + 1) == passantable)
+                        {
+                            if (endX == startX - 1 && endY == startY + 1)
+                            {
+                                passantable = (11, startX + 1);
+                                 
+                                return true;
+                            }
+                            else return false;
+                        }
+                        else return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
             {
                 return false;
             }
